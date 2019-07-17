f60fbd3b5b2a7871b5b7b2a517b12cbf26d5efaa
EED <42909006+watcharaz@users.noreply.github.com>
2019-07-18 01:31:20 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -80,22 +80,23 @@ else if ($message == "นับ 1-10"){
 else if ($message == "video"){
 	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
 	$arrayPostData['messages'][0]['type'] = "video";
-	$arrayPostData['messages'][0]['originalContentUrl'] = "https://www.youtube.com/watch?v=RJ68hwsE77Q";//ใส่ url ของ video ที่ต้องการส่ง
+	$arrayPostData['messages'][0]['originalContentUrl'] = "http://techslides.com/demos/sample-videos/small.webm";//ใส่ url ของ video ที่ต้องการส่ง
 	$arrayPostData['messages'][0]['previewImageUrl'] = "";//ใส่รูป preview ของ video
 	replyMsg($arrayHeader,$arrayPostData);
 }
-function replyMsg($arrayHeader, $arrayPostData)
-{
-    $strUrl = "https://api.line.me/v2/bot/message/reply";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $strUrl);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    curl_close($ch);
-}
+
+function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
 exit;
+?>
