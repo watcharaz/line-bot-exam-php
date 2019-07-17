047c3a355a27ef7c3373e5d9ce494373d508635c
EED <42909006+watcharaz@users.noreply.github.com>
2019-07-18 00:23:03 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'qHeas1kVDNRSBK0gHl3s8osjuqLp04RULKx8OK3/t1KXjrL0hVBl/OyYAUC2x0hTB4gpBG10FJFyV1GkFKf+45pXJURIwDAYlhG0L0maQttMyC7ypENzqNGskBbRs2k1J0SN5KTIFxRDRxTxFlKfwwdB04t89/1O/w1cDnyilFU=
+';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
